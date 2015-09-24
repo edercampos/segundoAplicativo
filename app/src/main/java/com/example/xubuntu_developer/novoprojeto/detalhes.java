abcb982a61d7db607498418076a4034901037cdf
xubuntu-developer <ricardo.olivioo@gmail.com>
2015-09-24 10:48:05 +0000

    Correção para incluir recebimento de intent na detalhes

--- app/src/main/java/com/example/xubuntu_developer/novoprojeto/detalhes.java
+++ app/src/main/java/com/example/xubuntu_developer/novoprojeto/detalhes.java
@@ -1,9 +1,15 @@
 package com.example.xubuntu_developer.novoprojeto;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.Arrays;
 
 
 @SuppressWarnings("deprecation")
@@ -13,27 +19,41 @@ public class detalhes extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalhes);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_detalhes, menu);
-        return true;
-    }
+        Intent intent = getIntent();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (intent.hasExtra(Intent.EXTRA_TEXT)) {
+            String detailText = intent.getStringExtra(Intent.EXTRA_TEXT);
+
+            TextView detailTextView = (TextView) findViewById(R.id.detalhe_item_texto);
+
+            detailTextView.setText(detailText);
+        }
+
+
+
+        }
+
+        @Override
+        public boolean onCreateOptionsMenu (Menu menu){
+            // Inflate the menu; this adds items to the action bar if it is present.
+            getMenuInflater().inflate(R.menu.menu_detalhes, menu);
             return true;
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        public boolean onOptionsItemSelected (MenuItem item){
+            // Handle action bar item clicks here. The action bar will
+            // automatically handle clicks on the Home/Up button, so long
+            // as you specify a parent activity in AndroidManifest.xml.
+            int id = item.getItemId();
+
+            //noinspection SimplifiableIfStatement
+            if (id == R.id.action_settings) {
+                return true;
+            }
+
+            return super.onOptionsItemSelected(item);
+        }
     }
-}
